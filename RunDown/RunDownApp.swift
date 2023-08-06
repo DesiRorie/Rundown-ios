8d8a8f87cae06ddd20144f288a7d6cee12d59c2b
DesiRorie <desirorie@icloud.com>
2023-08-05 23:59:59 -0400

    Initial Commit

---
+++ RunDown/RunDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunDownApp.swift
+//  RunDown
+//
+//  Created by Desi Rorie on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RunDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
